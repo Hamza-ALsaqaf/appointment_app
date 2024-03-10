8396e18658b995b72f921d56964e7415a23d3a82
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-11 03:50:40 +0530

    refactor: butter shift management

--- appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
+++ appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
@@ -6,4 +6,8 @@ from frappe.model.document import Document
 from frappe.utils import format_time
 class ScheduleShift(Document):
 	def before_save(self):
-		self.title=self.start_time+"-"+self.end_time
+		self.title=(
+			
+					format_time(self.start_time,"HH:mm")+"-"+format_time(self.end_time,"HH:mm")
+			
+			)
