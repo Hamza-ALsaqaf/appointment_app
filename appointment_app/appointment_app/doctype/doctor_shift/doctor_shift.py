6c8090fee9d2f6e8b0051f82f01c1967efcc306f
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-11 04:02:53 +0530

    Delete old doctor shift doctype

--- appointment_app/appointment_app/doctype/doctor_shift/doctor_shift.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Hamza H. Alsaqaf and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class DoctorShift(Document):
-	pass
