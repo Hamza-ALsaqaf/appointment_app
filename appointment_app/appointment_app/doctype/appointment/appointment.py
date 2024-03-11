79dde1c6264085f00cff92300c2dee716b412452
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-11 05:43:44 +0530

    added Appointment Queue and success portal page

--- appointment_app/appointment_app/doctype/appointment/appointment.py
+++ appointment_app/appointment_app/doctype/appointment/appointment.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2024, Hamza H. Alsaqaf and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Appointment(Document):
-	pass
+	def after_insert(self):
+		self.add_to_appointment_queue()
+	def add_to_appointment_queue(self):
+		q= frappe.get_doc("Appointment Queue",{
+			"date":self.date,
+			"shift":self.shift,
+			"clinic":self.clinic,
+		})
+		q.append("queue",{
+			"appointment":self.name,
+			"status":"pending"
+		})
+		q.save(ignore_permission=True)
+		return len(q.queue)
