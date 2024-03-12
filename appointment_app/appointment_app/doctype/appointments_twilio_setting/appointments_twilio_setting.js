e59afd4dc89133caa58818810ee6f7fbb416cf45
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-13 05:24:15 +0530

    Create Appointment Twilio Setting Single Doctype

---
+++ appointment_app/appointment_app/doctype/appointments_twilio_setting/appointments_twilio_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Hamza H. Alsaqaf and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Appointments Twilio Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ appointment_app/appointment_app/doctype/appointments_twilio_setting/test_appointments_twilio_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hamza H. Alsaqaf and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAppointmentsTwilioSetting(FrappeTestCase):
+	pass
